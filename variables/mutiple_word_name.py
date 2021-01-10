32950a86a1cfffd5042efe15441537e04c7781f4
LAPTOP-CPRVBEEL\newna <natchaphon.new2543@gmail.com>
2021-01-10 11:09:24 +0700

    add folder variables many...py mutiple...py and name.py

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,11 @@
+# Camel Case
+myVariableName = "Natchaphon Chaloemsong"
+
+# Pascal Case
+MyVariableName = "Natchaphon Chaloemsong"
+
+# Snake Case
+my_variable_name = "Natchaphon Chaloemsong"
+
+
+number_list = [1,2,3]
---
+++ variables/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "New"
+my_var = "New"
+_my_var_ = "New"
+myVar = "New"
+MYVR = "New"
+myvar2 = "New"
+
+#ไม่สามารถประกาศได้
+#2myvar = "New"
+my-var = "New"
+my var = "New"
