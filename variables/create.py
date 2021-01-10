33af33e2707aa6f104d34254a623c127ee9e4f9f
LAPTOP-CPRVBEEL\newna <natchaphon.new2543@gmail.com>
2021-01-10 10:15:26 +0700

    add new folder 3 folder single_and_double and variables

---
+++ variables/create.py
@@ -0,0 +1,6 @@
+name = "Nstchaphon Chaloemsong"
+age = 20
+weight = 68
+
+can_walk = True
+can_run = True
---
+++ variables/type.py
@@ -0,0 +1,12 @@
+name = "Nstchaphon Chaloemsong"
+age = 20
+weight = 68
+
+can_walk = True
+can_run = True
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
